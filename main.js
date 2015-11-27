f0854c5ddc4cb0e0a1a243f5211a41d6bf3d2d86
Dinius <edpb@edpb.net>
2015-11-27 08:47:25 +0100

    Added files.

---
+++ main.js
@@ -0,0 +1,79 @@
+var app 						= require('app'),
+		browserWindow 	= require('browser-window'),
+		globalShortcut	= require('global-shortcut'),
+		jQuery					= require('jquery'),
+		mainWindow 			= null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new browserWindow({
+	width: 1280, 
+	height: 720,
+	'title': 'Brain.fm',
+	webSecurity:false,
+	'node-integration': false
+  });
+	
+  mainWindow.setMenu(null);
+  mainWindow.loadURL('http://brain.fm');
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('app-command', function(e, cmd) {
+    if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
+      mainWindow.webContents.goBack();
+    }
+	else if (cmd === 'browser-forward' && mainWindow.webContents.canGoForward()) {
+      mainWindow.webContents.goForward();
+    }
+  });
+
+	// Global shortcuts..
+	globalShortcut.register('MediaNextTrack', function() {skip();});
+	globalShortcut.register('MediaStop', 			function() {playPause();}); // ?
+	globalShortcut.register('MediaPlayPause', function() {playPause();});
+	//globalShortcut.register('MediaPreviousTrack', function() {});
+
+	mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
+
+
+function skip()
+{
+	if (mainWindow == null) return;
+	// todo
+}
+
+
+var paused = false;
+/*
+* TODO
+* - Actually pause and resume it instead of the current function which is arbitrarily stopping it..
+* Might need to get the current playback status and set it back? Tired of reading that whole a\b\c\d source atm so maybe some other time.
+*
+* - Get the player's actual status, atm it just assumes it's playing the first time we do this.
+* */
+function playPause()
+{
+	if (mainWindow == null) return;
+
+	if (paused)
+	{
+		mainWindow.webContents.executeJavaScript('$("#jquery_jplayer").jPlayer("play", 0);$("#play_button").removeClass("tc_pause").addClass("tc_play");');
+	}
+	else
+	{
+		mainWindow.webContents.executeJavaScript('$("#jquery_jplayer").jPlayer("play", 1);$("#play_button").removeClass("tc_play").addClass("tc_pause");');
+	}
+
+	paused = !paused;
+}
+
+
+
