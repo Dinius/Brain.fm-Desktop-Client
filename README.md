ef3041307a76ebf43305580d703f0136cc340951
Dinius <e@dinius.me>
2016-10-09 22:19:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Brain.fm-Desktop-Client
+https://brain.fm Desktop client
+
+
+Simple Electron app as a desktop client for Brain.fm
+
+Runs in a standalone window with keyboard media button control for play/pause.
+
+(Currently just does stop\play, i.e. it can't pause.)
---
+++ main.js
@@ -0,0 +1,70 @@
+var app            = require('app'),
+		browserWindow  = require('browser-window'),
+		globalShortcut = require('global-shortcut'),
+		mainWindow     = null;
+
+app.on('window-all-closed', function () {
+	if (process.platform != 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('ready', function () {
+	mainWindow = new browserWindow({
+		width             : 1280,
+		height            : 720,
+		'title'           : 'Brain.fm',
+		webSecurity       : false,
+		'node-integration': false
+	});
+
+	mainWindow.setMenu(null);
+	mainWindow.loadURL('http://brain.fm');
+	//mainWindow.webContents.openDevTools();
+
+	mainWindow.on('app-command', function (e, cmd) {
+		if (cmd === 'browser-backward' && mainWindow.webContents.canGoBack()) {
+			mainWindow.webContents.goBack();
+		}
+		else if (cmd === 'browser-forward' && mainWindow.webContents.canGoForward()) {
+			mainWindow.webContents.goForward();
+		}
+	});
+
+	// Global shortcuts..
+	globalShortcut.register('MediaNextTrack', function () {skip();});
+	globalShortcut.register('MediaStop', function () {playPause();}); // ?
+	globalShortcut.register('MediaPlayPause', function () {playPause();});
+	//globalShortcut.register('MediaPreviousTrack', function() {});
+
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
+});
+
+
+function skip() {
+	if (mainWindow == null) {
+		return;
+	}
+	// todo
+}
+
+function playPause() {
+	if (mainWindow == null) {
+		return;
+	}
+
+	mainWindow.webContents.executeJavaScript('' +
+		'if ($("#play_button").hasClass("tc_pause")) {' +
+		'$("#jquery_jplayer").jPlayer("play", 0);' +
+		'$("#play_button").removeClass("tc_pause").addClass("tc_play");' +
+		'} else {' +
+		'$("#jquery_jplayer").jPlayer("play", 1);' +
+		'$("#play_button").removeClass("tc_play").addClass("tc_pause");' +
+		'}'
+	);
+}
+
+
+
